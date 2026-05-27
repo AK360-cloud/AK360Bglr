b55d87d91dc3aca12c3f25e19ab0883a58116e69
AK360-cloud <achuananthan335@gmail.com>
2026-05-27 09:01:48 +0530

    y

---
+++ tour/g001/data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-welcare-since1996",
+      "name": "WELCARE since1996",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.23841344024870814,
+        "pitch": -0.028222425108358706,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02814725255792183,
+          "pitch": 0.3565139778639761,
+          "rotation": 0,
+          "target": "1-welcare-since1996-equipments"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-welcare-since1996-equipments",
+      "name": "WELCARE since1996 Equipments",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.6889255496931899,
+        "pitch": 0.26727088962770473,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13654855672161048,
+          "pitch": 0.28510230722152485,
+          "rotation": 0,
+          "target": "2-welcare-since1996-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-welcare-since1996-reception",
+      "name": "WELCARE since1996 Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.0019199029272006,
+        "pitch": 0.2782064720037525,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.868408854741803,
+          "pitch": 0.3834419764668535,
+          "rotation": 0,
+          "target": "1-welcare-since1996-equipments"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
