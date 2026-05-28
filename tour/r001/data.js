e34cbd8ec716eabb1742e2c07047b87cb18db188
AK360-cloud <achuananthan335@gmail.com>
2026-05-28 11:23:20 +0530

    y

---
+++ tour/r001/data.js
@@ -0,0 +1,193 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-uruli-front-view_exported",
+      "name": "Uruli Front View_exported",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -3.1237227990925085,
+        "pitch": 0.04135465018887885,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.88831115332572,
+          "pitch": 0.28272099704206965,
+          "rotation": 0,
+          "target": "1-way-of-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-way-of-entry",
+      "name": "Way of Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.23914149604149593,
+        "pitch": 0.046980244569244434,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09185166453351812,
+          "pitch": 0.5035124813159904,
+          "rotation": 0,
+          "target": "2-interior-"
+        },
+        {
+          "yaw": -3.043672509822825,
+          "pitch": 0.5110933357912586,
+          "rotation": 4.71238898038469,
+          "target": "0-uruli-front-view_exported"
+        },
+        {
+          "yaw": 0.4888444841491282,
+          "pitch": 0.4373811194727377,
+          "rotation": 7.853981633974483,
+          "target": "0-uruli-front-view_exported"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-interior-",
+      "name": "Interior ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.5195600196389982,
+        "pitch": 0.32395330830552993,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.620627392457168,
+          "pitch": 0.589311342273442,
+          "rotation": 11.780972450961727,
+          "target": "3-interior-main_1"
+        },
+        {
+          "yaw": 0.37975142785916915,
+          "pitch": 0.8149645970183936,
+          "rotation": 5.497787143782138,
+          "target": "1-way-of-entry"
+        },
+        {
+          "yaw": -2.591991658343156,
+          "pitch": 0.6845727427635477,
+          "rotation": 3.141592653589793,
+          "target": "1-way-of-entry"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-interior-main_1",
+      "name": "Interior Main_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.9183288508863985,
+        "pitch": 0.12795589503228477,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08618180701437694,
+          "pitch": 0.5124647104856734,
+          "rotation": 5.497787143782138,
+          "target": "2-interior-"
+        },
+        {
+          "yaw": 2.389530600880825,
+          "pitch": 0.6009681249649521,
+          "rotation": 3.141592653589793,
+          "target": "2-interior-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
