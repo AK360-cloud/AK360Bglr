f4ec552532582ba9b07da1f8fcbebac8d1d17e80
AK360-cloud <achuananthan335@gmail.com>
2026-06-01 13:02:03 +0530

    y

---
+++ tour/b001/2BHK/data.js
@@ -0,0 +1,275 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry-1-bhk",
+      "name": "Entry 1 BHK",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.025183100465765662,
+        "pitch": -0.06447101819733092,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.36700154749818026,
+          "pitch": 0.3745859283435955,
+          "rotation": 0,
+          "target": "1-hall-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hall-",
+      "name": "Hall ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.406712641802482,
+        "pitch": 0.13771932242449303,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.521085389157923,
+          "pitch": 0.6345386580958134,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": 1.0334745407532697,
+          "pitch": 0.4265487548736253,
+          "rotation": 0,
+          "target": "5-balcony"
+        },
+        {
+          "yaw": 1.166352598427876,
+          "pitch": 0.5527764133645547,
+          "rotation": 1.5707963267948966,
+          "target": "3-bed-room"
+        },
+        {
+          "yaw": 0.9163271101760877,
+          "pitch": 0.5480488304163842,
+          "rotation": 4.71238898038469,
+          "target": "4-bathroom"
+        },
+        {
+          "yaw": -1.6403583789179326,
+          "pitch": 0.38197962488823833,
+          "rotation": 0,
+          "target": "0-entry-1-bhk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -3.000009385918137,
+        "pitch": 0.19211414624773226,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5153280960175213,
+          "pitch": 0.945363731742388,
+          "rotation": 6.283185307179586,
+          "target": "1-hall-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bed-room",
+      "name": "Bed Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.6000327867935322,
+        "pitch": 0.17077732245140176,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9131795081621377,
+          "pitch": 0.6795771777343873,
+          "rotation": 0,
+          "target": "1-hall-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bathroom",
+      "name": "Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.3446761695488423,
+        "pitch": 0.7017758819930862,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6861553870637422,
+          "pitch": 0.6582184163353979,
+          "rotation": 4.71238898038469,
+          "target": "1-hall-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-balcony",
+      "name": "Balcony",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3360549574627889,
+          "pitch": 0.9062640527505472,
+          "rotation": 0,
+          "target": "1-hall-"
+        },
+        {
+          "yaw": 0.48051419968979303,
+          "pitch": 0.649219819032961,
+          "rotation": 1.5707963267948966,
+          "target": "4-bathroom"
+        },
+        {
+          "yaw": 0.18250907021591622,
+          "pitch": 0.6256089207240727,
+          "rotation": 4.71238898038469,
+          "target": "3-bed-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "BMK 1 BHK",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
